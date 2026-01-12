da3be2aee230acd0c54eb03968dad3e852328582
kiouv <c.magnin@gmail.com>
2026-01-12 22:29:45 +0000

    init

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1170.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function(){
+	UAGBTableOfContents._run( {"mappingHeaders":[true,true,false,false,false,false],"scrollToTop":true,"makeCollapsible":false,"enableCollapsableList":false,"initialCollapse":false,"markerView":"disc","isFrontend":true,"initiallyCollapseList":false}, '.uagb-block-c06d3fe7' );
+} );
+ });
---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1199.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function(){
+	UAGBTableOfContents._run( {"mappingHeaders":[true,true,true,true,true,true],"scrollToTop":true,"makeCollapsible":false,"enableCollapsableList":false,"initialCollapse":false,"markerView":"disc","isFrontend":true,"initiallyCollapseList":false}, '.uagb-block-af37b79b' );
+} );
+ });
